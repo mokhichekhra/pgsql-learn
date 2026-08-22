3395e2156e72fea6df52240d2d0897bf0a4079f5
Ilfat Islamgaliev <ilfatislamgaliev@gmail.com>
2026-08-22 15:13:12 +0200

    added tasks

--- Lessons/Lesson 5/practice.sql
+++ Lessons/Lesson 5/practice.sql
@@ -17,9 +17,9 @@ FROM products --
 -- units_in_stock
 -- remaining_stock
 --  Don't worry if some results become negative.
-
-SELECT product_name,
-       units_in_stock,
+ 
+SELECT product_name, 
+       units_in_stock, 
        units_in_stock - 10 AS remaining_stock
 FROM products --
  -- Task 4 — Double the stock
@@ -28,8 +28,51 @@ FROM products --
  -- product_name
 -- units_in_stock
 -- new_stock
-
-SELECT product_name,
-       units_in_stock,
+ 
+SELECT product_name, 
+       units_in_stock, 
        units_in_stock * 2 AS new_stock
-FROM products
+FROM products --********
+ -- Task 5 — Modulo
+ -- Find the remainder when units_in_stock is divided by 5.
+ -- Return:
+ -- product_name
+-- units_in_stock
+-- remainder
+ --********
+ -- Task 6 — Increase product prices
+ -- Assume every product price increases by 10%.
+ -- Calculate the new price.
+ -- Return:
+ -- product_name
+-- unit_price
+-- new_price
+--**********************
+-- Task 7 — 20% discount
+ -- Calculate the price after applying a 20% discount to every product.
+ -- Return:
+ -- product_name
+-- unit_price
+-- discount_amount
+-- price_after_discount
+--*********************
+-- Task 8 — Final order-line amount
+ -- Calculate how much the customer actually pays after the discount.
+ -- Conceptually:
+ -- (unit_price × quantity) - discount_amount
+ -- Return:
+ -- order_id
+-- product_id
+-- unit_price
+-- quantity
+-- discount
+-- final_amount
+--******************************
+-- Task 9 — POWER
+ -- Calculate the square of every product's unit_price.
+ -- Use:
+ -- POWER()
+ -- Return:
+ -- product_name
+-- unit_price
+-- price_squared
